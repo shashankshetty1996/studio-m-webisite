717e9424486d7206670ead14e9bf336b30425f2a
Shashank Shetty <smile.shashank.s.shetty1996@gmail.com>
2021-06-27 18:17:30 +0530

    Footer mobile responsice fix

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -6,12 +6,12 @@ const Footer: FC<{}> = () => {
   const year = new Date().getFullYear();
   return (
     <footer className="bg-white py-4">
-      <div className="container mx-auto flex items-center justify-between text-gray-700">
+      <div className="container mx-auto md:flex text-center items-center justify-between text-gray-700">
         <p className="text-xs">
           &#169;Copyright {year}, Studio M. All Right Reserved.
         </p>
-        <div className="flex items-center">
-          <span className="text-xs">Get in touch with us: </span>
+        <div className="flex items-center justify-center md:justify-self-start">
+          <p className="text-xs">Get in touch with us: </p>
           <SocialMedia />
         </div>
       </div>
